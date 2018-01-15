38c5c13f98726aa8915a3055d6dc7530581cc2ea
ondercalbay <ondercalbay.com>
2018-01-15 09:17:20 +0300

    hakkimizda şndex rezerve

---
+++ Sanlilar.WebUITermalOtel/Scripts/Rezervasyon.js
@@ -0,0 +1,49 @@
+﻿
+function RezervasyonGonder() {
+    var Ad = $("#txtAd").val();
+    var Eposta = $("#txtEposta").val();
+    var Telefon = $("#txtTelefon").val();
+    var GirisTarihi = $("#txtGirisTarihi").val();
+    var CikisTarihi = $("#txtCikisTarihi").val();    
+
+    Data = 'action=RezervasyonGonder&Ad=' + Ad + 'Ad=' + Ad;
+
+    $.ajax({
+        cache: false,
+        global: false,
+        async: true,
+        type: 'POST',
+        dataType: "json",
+        data: Data,
+        url: '../ajax.aspx',
+        beforeSend: function () {
+            WaitingShow();
+        },
+        success: function (veri) {
+            try {
+                if (veri.hataId == 0) {
+                    $('#mesaj_' + mesajId).remove();
+                    var mesajAdet = $('#spnMesajAdet').html();
+                    if (mesajAdet != "" && mesajAdet > 1) {
+                        $('#spnMesajAdet').html(mesajAdet - 1);
+                    }
+                    else {
+                        $('#spnMesajAdet').html("");
+                    }
+                } else {
+                    Mesaj(veri.hataStr);
+                }
+                $('#wait').hide();
+            } catch (e) {
+                $('#wait').hide();
+                Mesaj(e.Message);
+            }
+        },
+        error: function (hata) {
+            Hata(hata);
+        },
+        complete: function () {
+            WaitingHide();
+        }
+    });
+}
